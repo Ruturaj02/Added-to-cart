a5072ece13b0cf065cb7f1b2dc8463a630ae1c8a
Ruturaj02 <121890966+Ruturaj02@users.noreply.github.com>
2024-08-05 17:11:48 +0530

    cards data fetching and till add to cart button its completed

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import CardsData from "./CardsData";
+import "./style.css"
 
 export default function Cards() {
   const [data, setData] = useState(CardsData);
@@ -9,19 +10,21 @@ export default function Cards() {
   return (
     <div className="container mt-3">
       <h2 className="text-center">Add to Cart Projects</h2>
-      <div className="row">
+      <div className="row d-flex justify-content-center align-item-center ">
         {data.map((element, id) => {
           return (
             <>
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
+              <Card style={{ width: "22rem",border:"none" }} className="mx-2 mt-4 card_style ">
+                <Card.Img variant="top" src={element.imgdata} style={{height:"16rem"}} className="mt-3" />
                 <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
+                  <Card.Title>{element.rname}</Card.Title>
                   <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
+                    Price:₹{element.price}
                   </Card.Text>
-                  <Button variant="primary">Go somewhere</Button>
+                  <div className="button_div d-flex justify-content-center ">
+                  <Button variant="primary" className="col-lg-12" >Add to cart</Button>
+                  </div>
+                 
                 </Card.Body>
               </Card>
             </>
